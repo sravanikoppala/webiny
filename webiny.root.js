b1d57f9c24665f4d98f21d5e06706052ab9641e3
Sravani Koppala <sk0142@uah.edu>
2021-01-18 12:02:38 -0600

    first commit

---
+++ webiny.root.js
@@ -0,0 +1,18 @@
+module.exports = {
+    template: "@webiny/cwp-template-full@4.14.0",
+    projectName: "new-webpage",
+    cli: {
+        plugins: [
+            require("@webiny/cli-plugin-deploy-components")(),
+            require("@webiny/cwp-template-full/hooks/api")(),
+            require("@webiny/cwp-template-full/hooks/apps")(),
+            require("@webiny/cli-plugin-scaffold"),
+            require("@webiny/cli-plugin-scaffold-graphql-service"),
+            require("@webiny/cli-plugin-scaffold-lambda"),
+            require("@webiny/cli-plugin-scaffold-admin-app-module"),
+            require("@webiny/cli-plugin-scaffold-node-package"),
+            require("@webiny/cli-plugin-scaffold-react-package"),
+            require("@webiny/cli-plugin-scaffold-react-app")
+        ]
+    }
+};
